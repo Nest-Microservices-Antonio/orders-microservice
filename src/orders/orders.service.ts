335da2c2910b0c29db9e35bb7bc328372bd829b9
Antonio Espinoza <anespinoza@baufest.com>
2024-04-19 17:40:33 -0400

    add nats server communication

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -10,7 +10,7 @@ import { PrismaClient } from '@prisma/client';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
 import { ChangeOrderStatusDto } from './dto';
-import { PRODUCT_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { firstValueFrom, throwError } from 'rxjs';
 
 @Injectable()
@@ -18,7 +18,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('OrdersService');
 
   constructor(
-    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy,
   ) {
     super();
   }
@@ -32,15 +32,15 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     try {
       //1 Confirmar los ids de los productos
       const productIds = createOrderDto.items.map((item) => item.productId);
-      const products: any[] = await firstValueFrom( //este rxjs nos permite transformar el observable en promise
-        this.productsClient.send({ cmd: 'validate_products' }, productIds), //Observable
+      const products: any[] = await firstValueFrom(
+        this.client.send({ cmd: 'validate_products' }, productIds),
       );
 
       //2. Cálculos de los valores
       const totalAmount = createOrderDto.items.reduce((acc, orderItem) => {
-        const price = products.find( //confiamos el precio de la bd de productos
+        const price = products.find(
           (product) => product.id === orderItem.productId,
-        ).price; //obtenemos el precio
+        ).price;
         return price * orderItem.quantity;
       }, 0);
 
@@ -55,8 +55,8 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
           totalItems: totalItems,
           OrderItem: {
             createMany: {
-              data: createOrderDto.items.map((orderItem) => ({ //barrido de los productos 
-                price: products.find( //Obtener el valor de la BD de products
+              data: createOrderDto.items.map((orderItem) => ({
+                price: products.find(
                   (product) => product.id === orderItem.productId,
                 ).price,
                 productId: orderItem.productId,
@@ -65,7 +65,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
             },
           },
         },
-        include: { // incluye información del order item creado
+        include: {
           OrderItem: {
             select: {
               price: true,
@@ -76,12 +76,12 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
         },
       });
 
-      return { //respuesta del ms, incluimos el nombre del producto trayendolo de la BD de products
+      return {
         ...order,
         OrderItem: order.OrderItem.map((orderItem) => ({
           ...orderItem,
           name: products.find((product) => product.id === orderItem.productId)
-            .name
+            .name,
         })),
       };
     } catch (error) {
@@ -141,15 +141,15 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
 
     const productIds = order.OrderItem.map((orderItem) => orderItem.productId);
     const products: any[] = await firstValueFrom(
-      this.productsClient.send({ cmd: 'validate_products' }, productIds),
+      this.client.send({ cmd: 'validate_products' }, productIds),
     );
 
     return {
       ...order,
       OrderItem: order.OrderItem.map((orderItem) => ({
         ...orderItem,
-        name: products.find((product) => product.id === orderItem.productId) //recupero solo el nombre del producto.
-          .name
+        name: products.find((product) => product.id === orderItem.productId)
+          .name,
       })),
     };
   }
